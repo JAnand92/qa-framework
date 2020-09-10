d5b7bb218f8a5350433ff63d12919dccb36e951d
Jayesh Anand <anandjayesh.qa@gmail.com>
2020-09-10 17:37:53 +0100

    changed structure

--- src/test/gui/java/pages/BasePage.java
+++ src/test/gui/java/pages/BasePage.java
@@ -1,8 +1,13 @@
 package pages;
 
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import tests.BaseTest;
 
 public class BasePage extends BaseTest {
@@ -43,6 +48,34 @@ public class BasePage extends BaseTest {
     public void enter(WebElement element, String value) {
         try {
             element.sendKeys(value);
+        } catch (Exception e) {
+            System.out.println(e.fillInStackTrace());
+        }
+    }
+
+    public boolean isAlertPresent() {
+        try {
+            Wait wait = new WebDriverWait(driver, 610);
+            Alert a = (Alert) wait.until(ExpectedConditions.alertIsPresent());
+            a.accept();
+            return  true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /*Login to application
+    * wait for alert
+    * if alert is present switch to it or send exception*/
+
+    public void verifyAlertBoxAfterSessionOut() {
+        try {
+            /*loginToApplication()*/
+            if(isAlertPresent()) {
+                System.out.println("Alert box is present after session.");
+            } else {
+                Assert.fail("Alert box is not present after session.");
+            }
         } catch (Exception e) {
             System.out.println(e.fillInStackTrace());
         }
